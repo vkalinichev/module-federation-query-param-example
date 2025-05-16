984792e802cc58918d32465fd9afb142299091ef
Vladimir Kalinichev <me@vkalinichev.com>
2025-05-16 14:36:20 +0300

    reduce

--- remote/src/App.tsx
+++ remote/src/App.tsx
@@ -1,12 +1,9 @@
-import { RemoteComponent } from "./components/RemoteComponent";
 import "./App.css";
 
 export default function App() {
   return (
     <div className="remote-app">
       <h3>Remote App</h3>
-
-      <RemoteComponent />
     </div>
   );
 }
--- remote/src/components/RemoteComponent.tsx
+++
@@ -1,9 +0,0 @@
-import "./RemoteComponent.css";
-
-export function RemoteComponent() {
-  return (
-    <div className="remote-component">
-      <h3>Remote Component</h3>
-    </div>
-  );
-}
